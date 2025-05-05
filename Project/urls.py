74ea14c4fcaf5c3c1e90de16ab783fb996bc4787
Mr-Prakash6381 <prakash875478@gmail.com>
2025-05-05 08:27:47 +0530

    Feat : Add the Second_day model urls link

--- Project/urls.py
+++ Project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('firstday/',include('first_day.urls')),
+    path('secondday/',include('second_day.urls')),
 ]
